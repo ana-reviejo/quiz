a93931e58cd8989cd2e35fa07bd0fb28276717ba
ana-reviejo <anita.rj93@hotmail.com>
2015-05-14 12:58:24 +0200

    Añadir Preguntas

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,22 @@ var quizController = require('../controllers/quiz_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
+
 /* GET author page. */
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Quiz' });
 });
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
 
 module.exports = router;
