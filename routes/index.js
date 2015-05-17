2e0ea25f3b57ddde839c9f4ca250de9417baa0ea
ana-reviejo <anita.rj93@hotmail.com>
2015-05-17 19:25:02 +0200

    moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/author', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId, commentController.load); //autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -31,4 +32,9 @@ router.post('/quizes/create',              sessionController.loginRequired, quiz
 router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
+
+//Definición de rutas de comentarios
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 module.exports = router;
